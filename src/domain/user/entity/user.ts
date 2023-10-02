0235e02aff1bd89c69e8679c94fa9d67f709c58b
Thiago Rocha <thiago.rocha@farmaciasapp.com.br>
2023-10-02 10:44:39 -0300

    fix: change files with eslint

--- src/domain/user/entity/user.ts
+++ src/domain/user/entity/user.ts
@@ -1,5 +1,5 @@
-import { Entity } from '../../../@seedwork';
-import UniqueEntityId from '../../../@seedwork/uniqueEntityId.vo';
+import { Entity } from '../../../@seedwork'
+import type UniqueEntityId from '../../../@seedwork/uniqueEntityId.vo'
 
 export interface UserProps {
   user_id?: string
@@ -23,7 +23,7 @@ export default class User extends Entity<UserProps> {
   is_admin: boolean
   created_at?: Date
   updated_at: Date | null
-  constructor(props: UserProps, id?: UniqueEntityId) {
+  constructor (props: UserProps, id?: UniqueEntityId) {
     super(props, id)
     this.user_id = this._id ?? id
     this.name = props.name
@@ -36,35 +36,35 @@ export default class User extends Entity<UserProps> {
     this.updated_at = props.updated_at ?? null
   }
 
-  changeName(name: string) {
+  changeName (name: string): void {
     this.name = name
   }
 
-  changeDocument(document: string) {
+  changeDocument (document: string): void {
     this.document = document
   }
 
-  changePhone(phone: string) {
+  changePhone (phone: string): void {
     this.phone = phone
   }
 
-  changeEmail(email: string) {
+  changeEmail (email: string): void {
     this.email = email
   }
 
-  changePassword(password: string) {
+  changePassword (password: string): void {
     this.password = password
   }
 
-  adminActivate() {
+  adminActivate (): void {
     this.is_admin = true
   }
 
-  adminDeactivate() {
+  adminDeactivate (): void {
     this.is_admin = false
   }
 
-  static create(props: UserProps, id?: UniqueEntityId): User {
+  static create (props: UserProps, id?: UniqueEntityId): User {
     return new User(props, id!)
   }
 }
--- src/domain/vacancies/entity/vacancy.ts
+++ src/domain/vacancies/entity/vacancy.ts
@@ -1,13 +1,13 @@
-import { Entity } from '../../../@seedwork';
-import UniqueEntityId from '../../../@seedwork/uniqueEntityId.vo';
+import { Entity } from '../../../@seedwork'
+import type UniqueEntityId from '../../../@seedwork/uniqueEntityId.vo'
 
 export interface VacanciesProps {
   vacancy_id?: string
-  title: string,
-  description: string,
-  requirements: string,
-  type: string,
-  is_active?: boolean,
+  title: string
+  description: string
+  requirements: string
+  type: string
+  is_active?: boolean
   created_at?: Date
 }
 
@@ -22,7 +22,7 @@ export default class Vacancies extends Entity<VacanciesProps> {
   is_active?: boolean
   created_at?: Date
 
-  constructor(props: VacanciesProps, id?: UniqueEntityId) {
+  constructor (props: VacanciesProps, id?: UniqueEntityId) {
     super(props, id)
     this.vacancy_id = this._id ?? id
     this.title = props.title
@@ -33,31 +33,31 @@ export default class Vacancies extends Entity<VacanciesProps> {
     this.created_at = props.created_at ?? new Date()
   }
 
-  changeTitle(title: string) {
+  changeTitle (title: string): void {
     this.title = title
   }
 
-  changeRequirements(requirements: string) {
+  changeRequirements (requirements: string): void {
     this.requirements = requirements
   }
 
-  changeType(type: string) {
+  changeType (type: string): void {
     this.type = type
   }
 
-  changeDescription(description: string) {
+  changeDescription (description: string): void {
     this.description = description
   }
 
-  activate() {
+  activate (): void {
     this.is_active = true
   }
 
-  deactivate() {
+  deactivate (): void {
     this.is_active = false
   }
 
-  static create(props: VacanciesProps, id?: UniqueEntityId): Vacancies {
+  static create (props: VacanciesProps, id?: UniqueEntityId): Vacancies {
     return new Vacancies(props, id)
   }
 }
